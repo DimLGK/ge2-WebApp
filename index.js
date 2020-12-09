719df774fc2da27fe7cc243cee7c22775a6f0c3e
Leuteris Mitilineos <std145287@ac.eap.gr>
2020-12-09 16:53:14 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+// Api for google maps
+
+        // Initialize and add the map
+        function initMap() {
+            const thessaloniki = { lat: 40.627470, lng: 22.948031 };
+            // The map, centered at Uluru
+            const map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 12,
+                center: thessaloniki,
+            });
+            // The marker, positioned at Uluru
+            const marker = new google.maps.Marker({
+                position: thessaloniki,
+                map: map,
+            });
+        }
+
+        var pmData = [
+            {
+                "name": "Χριστίνα Βασιλειάδη",
+                "message": "Ο πελάτης θα περιμένει στην Καμάρα στις 10:00."
+            }
+        ];
+        
+        // Configuration Firebase
+        var firebaseConfig = {
+            apiKey: "AIzaSyD_xg06CLNiORbmfVO-6vURoADclO1g2eo",
+            authDomain: "tourexploration.firebaseapp.com",
+            databaseURL: "https://tourexploration.firebaseio.com",
+            projectId: "tourexploration",
+            storageBucket: "tourexploration.appspot.com",
+            messagingSenderId: "115899410318",
+            appId: "1:115899410318:web:49b5df20824212d6d6a6df",
+            measurementId: "G-EMZ6NWJ72Y"
+        };
+
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+
+        // var markersRef = firebase.database().ref('markers'); 
+        // markersRef.on('value', snapshot => {
+        //   snapshot.forEach(function(childSnapshot) {
+        //     var childData = childSnapshot.val();
+
+        //   });     
+        // });
