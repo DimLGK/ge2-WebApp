834aacd2165142e6c1bc1c1eb05aa61309ba7e0e
Leuteris Mitilineos <std145287@ac.eap.gr>
2021-01-08 20:43:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -84,49 +84,8 @@ function closeChat() {
     localStorage.removeItem('chat');
 }
 
-var input = document.getElementById('christina');
-document.getElementById('submit').onclick = function () {
-    pmData.push(input.value);
-    screen.innerHTML = input.value;
-};
-
-// Configuration Firebase
-var firebaseConfig = {
-    apiKey: "AIzaSyD_xg06CLNiORbmfVO-6vURoADclO1g2eo",
-    authDomain: "tourexploration.firebaseapp.com",
-    databaseURL: "https://tourexploration.firebaseio.com",
-    projectId: "tourexploration",
-    storageBucket: "tourexploration.appspot.com",
-    messagingSenderId: "115899410318",
-    appId: "1:115899410318:web:49b5df20824212d6d6a6df",
-    measurementId: "G-EMZ6NWJ72Y"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Api for google maps
-
-// Initialize and add the map
-//function initMap() {
-//    const thessaloniki = { lat: 40.627470, lng: 22.948031 };
-//    const map = new google.maps.Map(document.getElementById("map"), {
-//        zoom: 12,
-//        center: thessaloniki,
-//    });
-    // The marker, positioned at Uluru
-//    const marker = new google.maps.Marker({
-//        position: thessaloniki,
-//        map: map,
-//    });
-// }
-
-//var pmData = [
-//   {
-//        "name": "Χριστίνα Βασιλειάδη",
-//        "message": "Ο πελάτης θα περιμένει στην Καμάρα στις 10:00."
-//    }
-//];
-
-
-
+// var input = document.getElementById('christina');
+// document.getElementById('submit').onclick = function () {
+//     pmData.push(input.value);
+//     screen.innerHTML = input.value;
+// };
